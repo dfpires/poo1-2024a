20a55af8d08ee46bd0e10555c771645f6297f0a6
Daniel Pires <dfpires@gmail.com>
2024-05-08 19:35:55 -0300

    Add files via upload

---
+++ Netflix/src/main/java/unifacef/edu/netflix/model/entity/FilmeEntity.java
@@ -0,0 +1,19 @@
+package unifacef.edu.netflix.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity // classe que é uma entidade de banco de dados
+@Table(name="filme") // classe é uma tabela do banco de dados
+public class FilmeEntity {
+    @Id // chave primário
+    private Long id;
+    @Column(name="nome")
+    private String nome;
+    @Column(name="nota")
+    private float nota;
+    @Column(name="anoLancamento")
+    private int anoLancamento;
+}
